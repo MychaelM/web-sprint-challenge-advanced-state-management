2bf087c99f0278ee4e2dc4bba03c6db17a181b19
Mychael M <mychaelbmenges@gmail.com>
2020-10-25 17:35:42 -0600

    added redux, middleware, and react-redux. setup store and provider

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import "./index.css";
 import App from "./components/App";
+import { smurfReducer } from './reducers';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(smurfReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
