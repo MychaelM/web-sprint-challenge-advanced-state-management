4a61f2a08003f8165d546de751587e5b3155563d
Mychael M <mychaelbmenges@gmail.com>
2020-10-25 18:05:22 -0600

    added action to get smurf data and render smurfs

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from "react";
 import "./App.css";
+import { connect } from 'react-redux';
+import { getSmurfs } from '../actions';
+
 class App extends Component {
+  // constructor(props) {
+  //   super();
+  //   this.state = {
+  //     props: props
+  //   }
+  // }
+
   render() {
     return (
       <div className="App">
         <h1>SMURFS! W/Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <button onClick={this.props.getSmurfs} >Get Smurfs</button>
+        {this.props.smurfs.map(smurf => (
+          <div key={smurf.id}>
+            <h2>{smurf.name}</h2>
+            <p>{smurf.age}</p>
+            <p>{smurf.height}</p>
+          </div>
+        ))}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    smurfs: state.smurfs
+  }
+};
+
+const mapDispatchToProps = { getSmurfs };
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,9 +1,14 @@
 const initialState = {
-  smurfs: {}
+  smurfs: []
 };
 
 export const smurfReducer = (state = initialState, action) => {
   switch (action.type) {
+    case "FETCHING_SMURFS_SUCCESS":
+      return {
+        ...state,
+        smurfs: action.payload
+      }
 
     default:
       return state;
